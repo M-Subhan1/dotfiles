5ce536b573ae58c740c5af21e303ff524e9de7a2
subaru <m.subhanabbas@gmail.com>
2024-10-12 10:02:16 +0500

    feat: load sniprun lazy

--- .config/nvim/lua/subaru/plugins/sniprun.lua
+++ .config/nvim/lua/subaru/plugins/sniprun.lua
@@ -2,6 +2,7 @@ return {
 	"michaelb/sniprun",
 	branch = "master",
 	build = "sh install.sh",
+	event = "VeryLazy",
 	-- do 'sh install.sh 1' if you want to force compile locally
 	-- (instead of fetching a binary from the github release). Requires Rust >= 1.65
 	config = function()
