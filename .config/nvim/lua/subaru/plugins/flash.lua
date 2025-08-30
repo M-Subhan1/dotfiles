afe02fb6cafc8343bfaf3057c98af29dcd076f55
subaru <m.subhanabbas@gmail.com>
2025-08-30 20:04:40 +0500

    feat: configure flash

---
+++ .config/nvim/lua/subaru/plugins/flash.lua
@@ -0,0 +1,19 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		modes = {
+			char = {
+				jump_labels = true,
+			},
+		},
+	},
+	-- stylua: ignore
+    keys = {
+    { "s", mode = "o", function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = "o", function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  },
+}
